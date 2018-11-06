2271bf23a5b6f4dcb14492db347c04b829da7af6
archit1197 <archit1197@gmail.com>
2018-11-07 03:59:49 +0530

    final results

--- constants.py
+++ constants.py
@@ -5,4 +5,4 @@ c = 1
 PRINT_ITERATION = 10000
 converge_iterations = 1000
 epsilon_convergence = 1e-3
-eps_values = {'mdp-01.txt':5*0.51794, 'mdp-02.txt':1.9667, 'mdp-riverswim.txt':10*0.153096, 'mdp-CasinoLand.txt':10*1.235594, 'mdp-SixArms.txt':5*0.82568}
+eps_values = {'mdp-01.txt':5*0.51794, 'mdp-02.txt':1.9667, 'mdp-riverswim.txt':5*0.153096, 'mdp-CasinoLand.txt':5*1.235594, 'mdp-SixArms.txt':5*0.82568}
